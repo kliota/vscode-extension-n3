76460c92414c634a3fb09866685c1a537b5f4f33
Anushka Joshi <anushka.joshi@gmail.com>
2024-09-22 13:12:58 +0200

    Removal of Function which is not used

--- server/src/server.ts
+++ server/src/server.ts
@@ -373,7 +373,6 @@ async function fetchAndExtractParameters(url: string): Promise<{ xsdValues: stri
                         while ((simpleTypeMatch = simpleTypeRegex.exec(parameterBlock)) !== null) {
                             const position = simpleTypeMatch[1]; // 's' for subject, 'o' for object
                             const type = simpleTypeMatch[2]; // xsd:string, xsd:float, etc.
-                            //console.log(`Captured type: ${type} for position: ${position}`);
 
                             if (position === 'subject') {
                                 subjectTypes.add(type);
@@ -761,46 +760,6 @@ async function validateTextDocument(textDocument: TextDocument): Promise<void> {
 				
 				return [items, listItemTypes];
 			}
-		
-			function validate_variable_types(types: string[], expectedTypes: Set<string>, variableTypes: Record<string, string>): boolean {
-				let allTypesValid = true;
-			
-				for (const type of types) {
-					if (type === "variable") {
-						// Extract variable name from the types list
-						const variableName = types.find(item => item.startsWith("?"));
-			
-						if (variableName) {
-							// Get the expected type for the variable
-							const expectedTypeForVariable = variableTypes[variableName];
-			
-							if (expectedTypeForVariable) {
-								// Check if the expected type for the variable matches any of the expected types
-								if (expectedTypes.has(expectedTypeForVariable)) {
-									connection.console.log(`Variable ${variableName} (expected type: ${expectedTypeForVariable}) matches the expected xsd types.`);
-								} else {
-									connection.console.log(`Variable ${variableName} (expected type: ${expectedTypeForVariable}) does not match the expected xsd types: ${Array.from(expectedTypes).join(", ")}.`);
-									allTypesValid = false;
-								}
-							} else {
-								// Variable type is still unknown, defer the validation
-								connection.console.log(`Variable ${variableName} is still unresolved (unknown type).`);
-								allTypesValid = false; // Variable is not valid if its type is unknown
-							}
-						} else {
-							// No variable name was found in the types array
-							connection.console.log("No variable name found in types.");
-							allTypesValid = false;
-						}
-					} else if (!expectedTypes.has(type)) {
-						// Literal type does not match any of the expected types
-						connection.console.warn(`Literal type ${type} does not match expected xsd types: ${Array.from(expectedTypes).join(", ")}.`);
-						allTypesValid = false;
-					}
-				}
-			
-				return allTypesValid;
-			} 
 
 			/**
 			 * Extracts the potential variable types for each variable.
